1399c8adfbb151e1c00868cccb6fd23c038a5da4
vignesh@crazygoldfish.com <vigneshdark@gmail.com>
2024-11-08 18:05:54 +0530

    updates

--- cosine-test/code.py
+++ cosine-test/code.py
@@ -1,10 +1,11 @@
 import numpy as np
 
+
 def COE(local_grad, global_grad):
     local_mag = np.linalg.norm(local_grad)
     global_mag = np.linalg.norm(global_grad)
     t = (local_grad@global_grad) / (local_mag * global_mag)
-    if t > 0 :
+    if t > 0:
         return (t**2) + 1
     else:
         return -1*(t**2) + 1
